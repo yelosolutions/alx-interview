984a4dfc0efd7ef4aa0a7bf852db769c5980068f
yelosolutions <ronokamau@gmail.com>
2023-01-15 08:56:09 +0000

    Returns a list of lists of integers representing the Pascal's triangle of n

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 '''
-Returns a list of lists of integers representing the\
-Pascal's triangle of n
+Pascal's triangle
 '''
 
+
 def pascal_triangle(n):
-	if n <= 0:
-		return []
-	triangle  = [[1] * (i + 1) for i in range(n)]
-	for i in range(n):
-		for j in range(1, i):
-			triangle[i][j] = triangle[i-1][j-1] + \
-triangle[i-1][j]
-	return triangle
+    """
+    Returns a list of lists of integers representing\
+    the Pascal's triangle of n or an empty list if n <= 0
+    """
+    if n <= 0:
+        return []
+    triangle = [[1] * (i + 1) for i in range(n)]
+    for i in range(n):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+    return triangle
